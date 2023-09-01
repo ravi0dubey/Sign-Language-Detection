6245511b1164dd74108eadc24f53c1c84d04dcf0
Ravi0Dubey <ravi0dubey@gmail.com>
2023-08-31 21:15:35 -0400

    updating  template.py

--- template.py
+++ template.py
@@ -1,3 +1,6 @@
+# Purpose of Template.py is to create a folder structure of our project. Rather than manual creation it will create all folders which we will be using in our project.
+# Within each folders, it will create the filenames where we will be writing our code.
+
 import os
 from pathlib import Path
 import logging
@@ -7,7 +10,7 @@ logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 project_name = "signLanguage"
 
-
+# In list_of_files mention all the folders  and filenames within each folders which we want in our project
 list_of_files = [
     ".github/workflows/.gitkeep",
     "data/.gitkeep",
@@ -47,27 +50,30 @@ list_of_files = [
     "Dockerfile",
     "requirements.txt",
     "setup.py"
-
-
 ]
 
 
+
+# For loop to read complete folder list and then create folder struture and respetive file name in the folder
 for filepath in list_of_files:
     filepath = Path(filepath)
 
+# it will separate file name from the directory name. Example if we have path = "sign_language/pipeline/training_pipeline.py"
+# os.path.split(path) will store filedir and filename as ('sign_language/pipeline', 'training_pipeline.py') respectively.
     filedir, filename = os.path.split(filepath)
 
+# if File directory is not spaces then it will create the directory
     if filedir !="":
         os.makedirs(filedir, exist_ok=True)
         logging.info(f"Creating directory: {filedir} for the file {filename}")
 
-    
+# if filename does not exists then it will create the filename    
     if(not os.path.exists(filename)) or (os.path.getsize(filename) == 0):
         with open(filepath, 'w') as f:
             pass
             logging.info(f"Creating empty file: {filename}")
 
-    
+# Else it will log that file already exists    
     else:
         logging.info(f"{filename} is already created")
 
